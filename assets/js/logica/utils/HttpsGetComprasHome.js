62b862358a74e05162650c7e2c88fb6dbc2123d6
AndresWnj <andres.wnj@gmail.com>
2022-12-01 11:27:54 -0500

    quede home

--- assets/js/logica/utils/HttpsGetComprasHome.js
+++ assets/js/logica/utils/HttpsGetComprasHome.js
@@ -35,19 +35,22 @@ document.addEventListener('DOMContentLoaded', e => {
                 <div class="planess">
                 <div class="cardInversion" id="plan2">
                     
-                    <div>
+                   <div style="display: flex">
+                   <div style="width: 100%">
                       <br>
-                      <h3 style="margin-left: -180px">${formData1.HtmlHeader}</h3>
+                      <h3 style="margin-left: 0px; color: ${formData1.TextColorHeader}!important">${formData1.HtmlHeader}</h3>
                       <br>
-                      <h3 style="margin-left: -190px; color: red ;">${formData1.HtmlBody}</h3>
+                      <h3 style="margin-left: 0px; color: red ;">${formData1.HtmlBody}</h3>
                       <br>
-                      <b style="margin-left: -230px; color: ${formData1.TextColorBody2};">${formData1.HtmlBody2}</b>
+                      <b style="margin-left: 0px; color: ${formData1.TextColorBody2};">${formData1.HtmlBody2}</b>
                       <br>
                       <br>
                       <img src="${imagenes}" style="width: 40px; margin-left: 30px;">
                       <br>
                     </div>
-                
+                    <button onclick="comprar()" style="background: #d79d13; color: black; font-weight: bold; "class="btnComprar">Buy</button>
+                   </div>
+                    
                 </div>
               </div>
              `;
