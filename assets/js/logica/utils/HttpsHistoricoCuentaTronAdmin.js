2111590c1319de7607b4eaf2c193b306dde0fe59
kamiloPROO <fotocopiasa100@gmail.com>
2023-02-21 17:28:14 -0500

    consumos listar cuentas tron y historico cuentas

---
+++ assets/js/logica/utils/HttpsHistoricoCuentaTronAdmin.js
@@ -0,0 +1,84 @@
+function comprar(val) {
+
+    // alert("siuuuu")
+    // window.location.href = "comprar2ronda.html"
+    console.log(val);
+
+    const idHpta = val;
+    localStorage.setItem("IdCompraFinal", idHpta);
+
+    if (idHpta != ""){
+        window.location.href = "comprar2ronda.html"
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'server under maintenance',
+        })
+    }
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', e => {
+
+    var idcfCuenta = localStorage.getItem("idcfComprar");
+    var idCfUser = localStorage.getItem("idCfLogin");
+    var idToken = localStorage.getItem("tokenLogin");
+    var loading = document.getElementById('cargando');
+
+    
+    $.ajax({
+        type: "POST",
+        url: "https://appwollpay.app/Api/GetHistoricoCuentaTronAdmin",
+        data: { "IdCf": idCfUser, "Token": idToken, "Skip": 1, "Take": 3000 },
+        success: function (data) {
+
+            loading.style.display = 'none'
+
+            var formData = JSON.parse(data);
+
+
+            for (const compras of formData) {
+
+                var formData1 = JSON.parse(compras.Json);
+
+                var idDeLaCompra = formData1.Id;
+                localStorage.setItem("sacoid", idDeLaCompra);
+                
+                console.log(formData1);
+
+                const productList = document.getElementById("divHistorial");
+                const element = document.createElement("div")
+
+                element.innerHTML += `
+
+                <div class="cardInversionHistorico" id="plan2">
+                    
+                   <div class="containerHistoAdmin" >
+                     <a class= "valorparseAdmin" style="color:${formData1.ColorValor}">${formData1.ValorParse}</a>
+                     <a class= "saldoooAdmin" >${formData1.SaldoParse}</a>
+                     <a class= "fechaaaAdmin">${formData1.FechaParse}</a>
+                   </div>
+                    
+                </div>
+
+             `;
+
+                productList.appendChild(element)
+
+            }
+
+        },
+        error: function () {
+            loading.style.display = 'none'
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'server under maintenance',
+            })
+        }
+    });
+
+})
---
+++ assets/js/logica/utils/HttpsListarCuentasTronAdmin.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', e => {
+
+    var idcff = localStorage.getItem("idCfLogin");
+    var tokeen = localStorage.getItem("tokenLogin");
+
+    var loading = document.getElementById('cargando');
+
+
+
+    $.ajax({
+        type: "POST",
+        url: "https://appwollpay.app/Api/GetCuentasTronAdmin",
+        data: { "IdCf": idcff, "Token": tokeen, "Skip": 1, "Take": 1000 },
+        success: function (data) {
+
+
+            var formData = JSON.parse(data);
+
+            
+
+            // console.log(formData);
+
+
+            for (const LisTodo of formData) {
+
+                console.log(LisTodo);
+
+                
+                const productList = document.getElementById("product-list");
+                const element = document.createElement("div")
+
+                element.innerHTML += `
+                <div onclick="wallet()" class="cardAdmin"
+                style="background:url(./assets/img/fondotarjeta.jpg);background-repeat: no-repeat;background-size: cover;">
+                <div class="containerCard " id="containerCardTron">
+                    <br>
+                    <h3 id="respuestaReferencia" class="moverRefeAdmin">${LisTodo.Referencia}</h3>
+                    <br>
+                    <span id="respuestaNombreUser" class="moverUserAdmin">${LisTodo.NombreUsuario}</span>
+                    <br>
+                    <br>
+
+                    <div class="divTodoAdmin"  >
+
+                    <div class="divTRX">
+                    <h3 class="trxAdmin">TRX</h3>
+                    <h3 id="respuestaSaldoTRX" class="moversaldosTrxAdmin">${LisTodo.Saldo}</h3>
+                    </div>
+
+                    <div class="divWPAY">
+                    <h3 class="wplayAdmin">WPAY</h3>
+                    <h3 id="respuestaSaldoWplay" class="moversaldosAdmin">${LisTodo.SALDO_WPAY}</h3>
+                    </div>
+
+                    <div class="divUSDT">
+                    <h3 class="usdtAdmin">USDT</h3>
+                    <h3 id="respuestaSaldoUSDT" class="moversaldosAdmin">${LisTodo.SALDO_USDT}</h3>
+                    </div>
+
+                    
+                    </div>
+
+                    
+                    
+                    
+                    <br>
+                    <br>
+                    <div style="display: flex;">
+                        <h3 id="respuestaActualizada" class="moverfechasAdmin">${LisTodo.FechaCreacionParse}</h3>
+                        <h3 class="act">Creada</h3>
+                    </div>
+                </div>
+            </div> 
+        `;
+
+                productList.appendChild(element)
+            }
+
+    },
+        error: function () {
+            alert("Ha ocurrido un error, por favor vuelva a intentarlo")
+        }
+    });
+
+})
